7175fb2d2e690987ac818a80f72557b20a9de5d9
chithien12322 <90127844+chithien12322@users.noreply.github.com>
2024-04-16 22:20:53 +0700

    Update help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -12,12 +12,12 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setColor('#2b71ec')
       .setAuthor({
-        name: 'Im here to Help!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1175487983915376662/1175667506791325706/communication.png?ex=656c10b0&is=65599bb0&hm=e378f1b355a2401bcab504b08a0766001d6b7c090c91ce0a7a7a87c868feb955&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        name: 'Menu Help!',
+        iconURL: 'https://cdn.discordapp.com/attachments/1130361549622616175/1228672764878127194/Untitled_Project.gif?ex=662ce5ae&is=661a70ae&hm=36f8691708f8e065514b9b8185b0ada6faab62e08b8d07e934ffc2df9216dacc&', 
+        url: 'https://discord.gg/XgJm6u2XNs'
     })
      
-      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [RTX](https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A)**\n\n__**COMMANDS :**__ `)
+      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Ping:** ${botPing}ms\n> **\n\n__**COMMANDS :**__ `)
       .addFields(
         // Basic commands category
         {
@@ -57,23 +57,8 @@ module.exports = {
         }
       )
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
-      .setImage(`https://cdn.discordapp.com/attachments/1170652474563117138/1224660177039396876/MULTI_MUSIC_v1.5.png?ex=661e4cab&is=660bd7ab&hm=f7a542462fabaf9c1530ece5aa72597cff3ac032876bba46df5ddba7e122ea99&`);
-
-    const button1 = new ButtonBuilder()
-      .setLabel('YouTube')
-      .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
-      .setStyle(ButtonStyle.Link);
-
-    const button2 = new ButtonBuilder()
-      .setLabel('Discord')
-      .setURL('https://discord.gg/FUEHs7RCqz')
-      .setStyle(ButtonStyle.Link);
-
-    const button3 = new ButtonBuilder()
-      .setLabel('Code')
-      .setURL('https://replit.com/@BEASTGAMERS1/MULTI-MUSIC-BOT-v200?v=1')
-      .setStyle(ButtonStyle.Link);
-      
+      .setImage(`https://cdn.discordapp.com/attachments/1130361549622616175/1228672764878127194/Untitled_Project.gif?ex=662ce5ae&is=661a70ae&hm=36f8691708f8e065514b9b8185b0ada6faab62e08b8d07e934ffc2df9216dacc&`);
+   
     const row = new ActionRowBuilder()
       .addComponents(button1, button2, button3);
     
